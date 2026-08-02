663bcd4651be40b3f72038aceeb4e0af5bb945fd
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-08-02 07:23:19 +0500

    rewrite: configuration lead file

---
+++ modules/features/configuration/configuration.nix
@@ -0,0 +1,49 @@
+{
+    inputs,
+    pkgs,
+    lib,
+    hostName,
+    timeZone,
+    configDirectory,
+    locale,
+    disabledOSTargets,
+    ...
+}:
+
+{
+    imports = [
+        ../../hosts/${hostName}/default.nix
+        (inputs.import-tree ./modules)
+    ];
+
+    stylix.targets = lib.foldl' (
+        acc: target: lib.recursiveUpdate acc (lib.setAttrByPath [ target "enable" ] false)
+    ) { } disabledOSTargets;
+
+    environment.sessionVariables = {
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+        FLAKE_PATH = configDirectory;
+        LANG = locale;
+    };
+    fonts.packages = with pkgs; [
+        newcomputermodern
+    ];
+    environment.systemPackages = with pkgs; [
+        neovim-unwrapped
+        nix-output-monitor
+        zip
+        unzip
+        _7zz
+        file
+        gcc
+        fzf
+        tmux
+        seahorse
+    ];
+
+    time.timeZone = timeZone;
+    nixpkgs.config.allowUnfree = true;
+    system.stateVersion = "26.05";
+
+}
