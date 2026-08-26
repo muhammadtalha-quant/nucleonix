371bce426863f0722cc1a04dcf690bad8e8391c4
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-08-26 18:28:22 +0500

    hypr:conf: ghostty my terminal of choice

--- modules/features/dotfiles/raw/hypr/hyprland/keybindings/applications.lua
+++ modules/features/dotfiles/raw/hypr/hyprland/keybindings/applications.lua
@@ -2,19 +2,19 @@ local KEYS = require("hyprland.keybindings.keys")
 local helpers = require("hyprland.keybindings.helpers")
 
 local apps = {
-    [helpers.register(KEYS.MODIFIER.SUPER, KEYS.SPECIAL.ENTER)] = {cmd = "kitty",                 desc = "Open Ghostty Terminal"},
-    [helpers.register(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.B)]    = {cmd = "google-chrome",    desc = "Open Google Chrome Browser"},
-    [helpers.register(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.E)]    = {cmd = "nautilus",                desc = "Open File Manager"},
-    [helpers.register(KEYS.MODIFIER.SUPER, KEYS.PUNCTUATION.SEMICOLON)]    = {cmd = "localsend_app",           desc = "Open Localsend"}
+  [helpers.register(KEYS.MODIFIER.SUPER, KEYS.SPECIAL.ENTER)]         = { cmd = "ghostty", desc = "Open Ghostty Terminal" },
+  [helpers.register(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.B)]            = { cmd = "google-chrome", desc = "Open Google Chrome Browser" },
+  [helpers.register(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.E)]            = { cmd = "nautilus", desc = "Open File Manager" },
+  [helpers.register(KEYS.MODIFIER.SUPER, KEYS.PUNCTUATION.SEMICOLON)] = { cmd = "localsend_app", desc = "Open Localsend" }
 }
 
 
-for keybind,app in pairs(apps) do 
-     hl.bind(
-        keybind,
-        hl.dsp.exec_cmd(app.cmd),
-        {
-            description = app.desc,
-        }
-     )
+for keybind, app in pairs(apps) do
+  hl.bind(
+    keybind,
+    hl.dsp.exec_cmd(app.cmd),
+    {
+      description = app.desc,
+    }
+  )
 end
