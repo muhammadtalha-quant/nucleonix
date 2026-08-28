7c469ed3ff447e5ffd178c0d63981df2736b248c
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-08-28 18:23:49 +0500

    hypr:conf: reformat with stylua

--- modules/features/dotfiles/raw/hypr/hyprland/keybindings/windows.lua
+++ modules/features/dotfiles/raw/hypr/hyprland/keybindings/windows.lua
@@ -2,37 +2,37 @@ local KEYS = require("hyprland.keybindings.keys")
 local helpers = require("hyprland.keybindings.helpers")
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.ALPHABET.Q
-    ),
-    hl.dsp.window.close(),
-    {
-        description = "Gracefully Close Window"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.ALPHABET.Q
+  ),
+  hl.dsp.window.close(),
+  {
+    description = "Gracefully Close Window"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MODIFIER.SHIFT,
-        KEYS.ALPHABET.Q
-    ),
-    hl.dsp.window.kill(),
-    {
-        description = "Forcefully Close Window"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MODIFIER.SHIFT,
+    KEYS.ALPHABET.Q
+  ),
+  hl.dsp.window.kill(),
+  {
+    description = "Forcefully Close Window"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.ALPHABET.T
-    ),
-    hl.dsp.window.float(),
-    {
-        description = "Toggle Float/Tile"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.ALPHABET.T
+  ),
+  hl.dsp.window.float(),
+  {
+    description = "Toggle [Un]float"
+  }
 )
 
 hl.bind(
@@ -49,66 +49,79 @@ hl.bind(
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.ARROW.RIGHT
-    ),
-    hl.dsp.focus({
-        direction = "right"
-    }),
-    {
-        description = "Focus Window Right"
-    }
-)
-hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.ARROW.UP
-    ),
-    hl.dsp.focus({
-        direction = "up"
-    }),
-    {
-        description = "Focus Window Up"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.ARROW.LEFT
+  ),
+  hl.dsp.focus({
+    direction = "left"
+  }),
+  {
+    description = "Focus Window Left"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.ARROW.DOWN
-    ),
-    hl.dsp.focus({
-        direction = "down"
-    }),
-    {
-        description = "Focus Window Down"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.ARROW.RIGHT
+  ),
+  hl.dsp.focus({
+    direction = "right"
+  }),
+  {
+    description = "Focus Window Right"
+  }
+)
+hl.bind(
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.ARROW.UP
+  ),
+  hl.dsp.focus({
+    direction = "up"
+  }),
+  {
+    description = "Focus Window Up"
+  }
 )
 
-
 hl.bind(
-    helpers.register(
-     KEYS.MODIFIER.SUPER,
-     KEYS.ALPHABET.F
-    ),
-    hl.dsp.window.fullscreen(
-     {
-       mode = "maximized",
-       action = "toggle"
-     }
-    ),
-    {
-       description = "Window: Maximize" 
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.ARROW.DOWN
+  ),
+  hl.dsp.focus({
+    direction = "down"
+  }),
+  {
+    description = "Focus Window Down"
+  }
 )
 
 
 hl.bind(
   helpers.register(
-   KEYS.MODIFIER.SUPER,
-   KEYS.MODIFIER.SHIFT,
-   KEYS.ALPHABET.F
+    KEYS.MODIFIER.SUPER,
+    KEYS.ALPHABET.F
+  ),
+  hl.dsp.window.fullscreen(
+    {
+      mode = "maximized",
+      action = "toggle"
+    }
+  ),
+  {
+    description = "Window: Maximize"
+  }
+)
+
+
+hl.bind(
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MODIFIER.SHIFT,
+    KEYS.ALPHABET.F
   ),
   hl.dsp.window.fullscreen(),
   {
@@ -119,101 +132,102 @@ hl.bind(
 
 hl.bind(
   helpers.register(
-   KEYS.MODIFIER.SUPER,
-   KEYS.SYMBOL.BRACKETRIGHT
+    KEYS.MODIFIER.SUPER,
+    KEYS.SYMBOL.BRACKETRIGHT
   ),
   hl.dsp.layout("splitratio +0.1"),
   {
-    repeating = true
+    repeating = true,
+    description = "Increase Split Ratio By 10%",
   }
 )
 
 hl.bind(
   helpers.register(
-   KEYS.MODIFIER.SUPER,
-   KEYS.SYMBOL.BRACKETLEFT
+    KEYS.MODIFIER.SUPER,
+    KEYS.SYMBOL.BRACKETLEFT
   ),
   hl.dsp.layout("splitratio -0.1"),
   {
-    repeating = true
+    repeating = true,
+    description = "Decrease Split Ratio By 10%",
   }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MOUSE.LMB
-    ),
-    hl.dsp.window.drag(),
-    {
-        description = "Drag Window"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MOUSE.LMB
+  ),
+  hl.dsp.window.drag(),
+  {
+    description = "Drag Window"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MOUSE.RMB
-    ),
-    hl.dsp.window.resize(),
-    {
-        description = "Resize Window"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MOUSE.RMB
+  ),
+  hl.dsp.window.resize(),
+  {
+    description = "Resize Window"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MODIFIER.SHIFT,
-        KEYS.ARROW.LEFT
-    ),
-    hl.dsp.window.swap({
-        direction = "left"
-    }),
-    {
-        description = "Swap Focused Window with Window to Left"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MODIFIER.SHIFT,
+    KEYS.ARROW.LEFT
+  ),
+  hl.dsp.window.swap({
+    direction = "left"
+  }),
+  {
+    description = "Swap Focused Window with Window to Left"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MODIFIER.SHIFT,
-        KEYS.ARROW.RIGHT
-    ),
-    hl.dsp.window.swap({
-        direction = "right"
-    }),
-    {
-        description = "Swap Focused Window with Window to Right"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MODIFIER.SHIFT,
+    KEYS.ARROW.RIGHT
+  ),
+  hl.dsp.window.swap({
+    direction = "right"
+  }),
+  {
+    description = "Swap Focused Window with Window to Right"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MODIFIER.SHIFT,
-        KEYS.ARROW.UP
-    ),
-    hl.dsp.window.swap({
-        direction = "up"
-    }),
-    {
-        description = "Swap Focused Window with Window Above"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MODIFIER.SHIFT,
+    KEYS.ARROW.UP
+  ),
+  hl.dsp.window.swap({
+    direction = "up"
+  }),
+  {
+    description = "Swap Focused Window with Window Above"
+  }
 )
 
 hl.bind(
-    helpers.register(
-        KEYS.MODIFIER.SUPER,
-        KEYS.MODIFIER.SHIFT,
-        KEYS.ARROW.DOWN
-    ),
-    hl.dsp.window.swap({
-        direction = "down"
-    }),
-    {
-        description = "Swap Focused Window with Window Below"
-    }
+  helpers.register(
+    KEYS.MODIFIER.SUPER,
+    KEYS.MODIFIER.SHIFT,
+    KEYS.ARROW.DOWN
+  ),
+  hl.dsp.window.swap({
+    direction = "down"
+  }),
+  {
+    description = "Swap Focused Window with Window Below"
+  }
 )
-
