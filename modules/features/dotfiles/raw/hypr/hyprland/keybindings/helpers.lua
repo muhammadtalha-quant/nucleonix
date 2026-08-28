9dde6782a1161739ccaf04b311c4499252f520a8
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-08-28 18:25:09 +0500

    hypr:conf: add two new helper functions
    - one for constructing webapp commands
    - other for registering submaps with less boilerplates

--- modules/features/dotfiles/raw/hypr/hyprland/keybindings/helpers.lua
+++ modules/features/dotfiles/raw/hypr/hyprland/keybindings/helpers.lua
@@ -2,56 +2,63 @@ local KEYS = require("hyprland.keybindings.keys")
 local M = {}
 
 function M.register(...)
-    return table.concat({...}, "+")
+  return table.concat({ ... }, "+")
 end
 
-
-function M.focus_ws(workspace_id) 
-    hl.bind(
-        M.register(
-            KEYS.MODIFIER.SUPER,
-            tostring(workspace_id)
-        ),
-        hl.dsp.focus({
-            workspace = tostring(workspace_id)
-        }), 
-        {
-            description = "Switch To Workspace # " .. tostring(workspace_id)
-        }
-    )
+function M.focus_ws(workspace_id)
+  hl.bind(
+    M.register(
+      KEYS.MODIFIER.SUPER,
+      tostring(workspace_id)
+    ),
+    hl.dsp.focus({
+      workspace = tostring(workspace_id)
+    }),
+    {
+      description = "Switch To Workspace # " .. tostring(workspace_id)
+    }
+  )
 end
 
 function M.move_window_to_and_focus_ws(workspace_id)
-     hl.bind(
-        M.register(
-            KEYS.MODIFIER.SUPER,
-            KEYS.MODIFIER.SHIFT,
-            tostring(workspace_id)
-        ),
-        hl.dsp.window.move({
-            workspace = tostring(workspace_id)
-        }), 
-        {
-            description = "Move Window To Workspace # " .. tostring(workspace_id)
-        }
-    )
+  hl.bind(
+    M.register(
+      KEYS.MODIFIER.SUPER,
+      KEYS.MODIFIER.SHIFT,
+      tostring(workspace_id)
+    ),
+    hl.dsp.window.move({
+      workspace = tostring(workspace_id)
+    }),
+    {
+      description = "Move Window To Workspace # " .. tostring(workspace_id)
+    }
+  )
 end
 
-function M.move_window_to_ws(workspace_id) 
-    hl.bind(
-        M.register(
-            KEYS.MODIFIER.SUPER,
-            KEYS.MODIFIER.ALT,
-            tostring(workspace_id)
-        ),
-        hl.dsp.window.move({
-            workspace = tostring(workspace_id),
-            follow = false,
-        }), 
-        {
-            description = "Move Window (Silent) To Workspace # " .. tostring(workspace_id) 
-        }
-    )
+function M.move_window_to_ws(workspace_id)
+  hl.bind(
+    M.register(
+      KEYS.MODIFIER.SUPER,
+      KEYS.MODIFIER.ALT,
+      tostring(workspace_id)
+    ),
+    hl.dsp.window.move({
+      workspace = tostring(workspace_id),
+      follow = false,
+    }),
+    {
+      description = "Move Window (Silent) To Workspace # " .. tostring(workspace_id)
+    }
+  )
+end
+
+function M.launch_url_as_webapp(url)
+  return "google-chrome --app=" .. url
+end
+
+function M.register_submap(submap_name, submap_bindings)
+  hl.define_submap(submap_name, submap_bindings)
 end
 
 return M
