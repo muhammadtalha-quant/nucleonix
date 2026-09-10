a2330f1492c5ec2078f1b1f6b15def5021111386
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-10 21:42:39 +0500

    home-manager: pass userName as explicit param

--- modules/features/home-manager/decl.nix
+++ modules/features/home-manager/decl.nix
@@ -1,9 +1,9 @@
-{ userName, extraSpecialArgs }: {
+{ userName, extraSpecialArgs, ... }: {
   home-manager = {
     inherit extraSpecialArgs;
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.${userName} = import ./home.nix;
+    users.${userName} = import ./home.nix { inherit userName; };
     backupFileExtension = "backup";
   };
 }
