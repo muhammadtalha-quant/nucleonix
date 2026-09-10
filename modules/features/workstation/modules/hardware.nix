80ed84f157e9710b4ee07c5bb69e9798e9a39170
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-10 22:33:01 +0500

    hosts(hp-probook-430g2): add bluetooth declarations to host config

--- modules/features/workstation/modules/hardware.nix
+++ modules/features/workstation/modules/hardware.nix
@@ -1,9 +1,5 @@
 {
   hardware = {
-
-    bluetooth.enable = true;
-    bluetooth.powerOnBoot = true;
-
     enableAllFirmware = true;
     enableAllHardware = true;
   };
--- modules/hosts/hp-probook-430g2/default.nix
+++ modules/hosts/hp-probook-430g2/default.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }: {
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      intel-media-sdk
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+      ];
+    };
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
   };
   hardware.facter.reportPath = ./hardware_report.json;
 }
