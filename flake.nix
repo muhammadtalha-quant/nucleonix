58f3743fac332302209c1bdbfbc6d1db61948b26
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-10 21:20:45 +0500

    flake: mini refactorings for the introduced changes

--- flake.nix
+++ flake.nix
@@ -39,8 +39,6 @@
 
       # !=== USER CONFIG ===!
       realName = "Muhammad Talha";
-      emailAddress = "muhammadtalha.quant@gmail.com";
-      gpgKey = "33DF23031DE1A83C"; # public key
 
       # !=== DISKO CONFIG ===!
       storageDevice = "/dev/sda";
@@ -48,6 +46,16 @@
 
       # !=== ENVIRONMENT CONFIG ===!
       configDirectory = "/home/${userName}/nucleonix/";
+
+      # !=== HOME MANAGER ===!
+      extraSpecialArgs = {
+        inherit inputs;
+        inherit stylix;
+        inherit realName;
+        inherit stateVersion;
+        emailAddress = "muhammadtalha.quant@gmail.com";
+        gpgKey = "33DF23031DE1A83C";
+      };
 
       # !=== SYNCTHING CONFIG ===!
       devices = {
@@ -77,32 +85,32 @@
         in
         nixpkgs.lib.nixosSystem {
           inherit (hostHardware) system;
+          specialArgs = {
+            inherit userName;
+            inherit hashedRootPassword;
+            inherit hashedUserPassword;
+            inherit stateVersion;
+            inherit realName;
+            inherit hostName;
+            inherit timeZone;
+            inherit configDirectory;
+            inherit storageDevice;
+            inherit locale;
+            inherit swapSize;
+            inherit extraSpecialArgs;
+            inherit devices;
+            inherit folders;
+            inherit inputs;
+          };
+          modules = [
+            ./modules/common/nixos-core/core.nix
+            ./modules/features/workstation/workstation.nix
+            ./modules/hosts/${hostName}/default.nix
+            home-manager.nixosModules.home-manager
+            ./modules/features/home-manager/decl.nix
+            disko.nixosModules.disko
+            ./modules/common/disko/laptop.nix
+          ];
         };
-        modules = [
-          ./modules/common/nixos-core/core.nix
-          ./modules/features/workstation/workstation.nix
-          ./modules/hosts/${hostName}/default.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.${userName} = import ./modules/features/home-manager/home.nix;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                inherit inputs;
-                inherit userName;
-                inherit stylix;
-                inherit realName;
-                inherit stateVersion;
-                inherit gpgKey;
-                inherit emailAddress;
-              };
-            };
-          }
-          disko.nixosModules.disko
-          ./modules/common/disko/laptop.nix
-        ];
-      };
     };
 }
